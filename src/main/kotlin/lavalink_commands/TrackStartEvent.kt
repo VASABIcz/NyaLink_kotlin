281e1749ea46ddde18a70114ba1523936c151421
VASABIcz <vasabisroa@gmail.com>
2022-10-06 01:27:55 +0200

    guildId field

--- src/main/kotlin/lavalink_commands/TrackStartEvent.kt
+++ src/main/kotlin/lavalink_commands/TrackStartEvent.kt
@@ -3,4 +3,4 @@ package lavalink_commands
 import kotlinx.serialization.Serializable
 
 @Serializable
-class TrackStartEvent(val track: String)
+class TrackStartEvent(val track: String, val guildId: Long)
